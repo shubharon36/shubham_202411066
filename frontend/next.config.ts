c8023763aa0f4aed9e7384cced3300de7392d4b2
shubharon36 <107632596+shubharon36@users.noreply.github.com>
2025-10-23 21:02:56 +0530

    Create next.config.ts

---
+++ frontend/next.config.ts
@@ -0,0 +1,8 @@
+// frontend/next.config.ts
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: { ignoreDuringBuilds: true }, // <-- skip ESLint in `next build`
+};
+
+export default nextConfig;
